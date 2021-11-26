5ed4b001e08c46c26893a379c97d5e193cd70b69
Swatosj <pierre.gambier@yahoo.fr>
2021-11-26 15:42:11 +0100

    Ajout de commentaires + corrections

--- mtg_animate_functions.js
+++ mtg_animate_functions.js
@@ -1,39 +1,41 @@
-const ARRAY_BITMAPS = new Array();
-const EXTENSION_ID_TEST = "AFR";
+const ARRAY_BITMAPS = [];
 const SCRYFALL_URL = "https://api.scryfall.com/cards/";
-const TICKER = null;
 var stageCollection;
 
 // Initialise le canvas HTML
 // IMPORTANT : copier <script src="https://code.createjs.com/1.0.0/createjs.min.js"></script> avant d'importer ce script
 function init() 
 {
-    stageCollection = new createjs.Stage("canvasCollection");
+    stageCollection = new createjs.Stage("canvasCollection"); // ID du canvas HTML en paramètres
     var circle = new createjs.Shape();
-    var command = circle.graphics.beginFill("red").command;
+    var circleCommand = circle.graphics.beginFill("red").command;
 
     circle.graphics.drawCircle(0, 0, 50);
     circle.x = 900;
     circle.y = 100;
 
-    stageCollection.addChild(circle);  
+    stageCollection.addChild(circle); // On ajoute notre cercle à notre canvas
     stageCollection.update();
 
     circle.on("click", function() {
         getAndDrawCollection(stageCollection);  
-        command.style = "green";      
         stageCollection.update();
-        createjs.Ticker.on("tick", tick);
+        circleCommand.style = "green"; // On actualise la couleur du bouton         
+        createjs.Ticker.on("tick", tick); // Définit la fonction de rafraichissement temps réel
         createjs.Ticker.setFPS(30);	
     });	
 }
 
-function tick(event) {
-    var randomCoord; 
+// Fonction qui actualise l'interface en temps réel
+function tick(event) 
+{
+    var randomX, randomY; 
     ARRAY_BITMAPS.forEach(image => {
-        randomCoord = Math.random() * 25;
-        image.x += randomCoord;
-        image.y += randomCoord;
+        randomX = Math.random() * 25;
+        randomY = Math.random() * 20;
+        image.x += randomX;
+        image.y += randomY;
+
         if (image.x > stageCollection.canvas.width) {
             image.x = 0;
         }
@@ -44,6 +46,7 @@ function tick(event) {
     stageCollection.update(event); 
 }
 
+// Récupères la liste des URLs d'images via AJAX et créer de nouvelles images avant de les ajouter à la scène
 function getAndDrawCollection(stage) 
 {
     var xhttp = new XMLHttpRequest();   
@@ -57,19 +60,19 @@ function getAndDrawCollection(stage)
                 if (urlImage != null && urlImage != "") 
                 {
                     console.log(SCRYFALL_URL + urlImage + "?format=image");
-                    var image = new createjs.Bitmap(SCRYFALL_URL + urlImage + "?format=image");          
+                    let image = new createjs.Bitmap(SCRYFALL_URL + urlImage + "?format=image");          
                     image.x = xCoord;
                     image.y = xCoord;
-                    image.scale = 0.3;   
+                    image.scale = 0.3; // Mise à l'échelle 30% de la taille d'origine  
                     xCoord += 100;
 
-                    ARRAY_BITMAPS.push(image);
-                    stage.addChild(image); 
+                    ARRAY_BITMAPS.push(image); // Sauvegarde de la référence de l'image
+                    stage.addChild(image); // Ajoute l'image à la scène
                 }
             });
             return arrayUrls;
         }
     };  
-    xhttp.open("GET", "getImagesUrlFromExtension.php", true); // On appelle le fichier showExtensionImages.php en GET avec le paramètre extensionId 
+    xhttp.open("GET", "getImagesUrlFromExtension.php", true); //AJAX OP
     xhttp.send();
 }
